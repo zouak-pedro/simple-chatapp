f29cd38645cd1a86c63eff4e080087a6f92a2c61
zouak-pedro <zouakpedro@gmail.com>
2025-01-21 17:55:34 +0100

    add event lesener by keyword, and check for the name befor start the app

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const ws = new WebSocket("ws://localhost:8080");
 const chat = document.getElementById("chat");
-const usernameInput = document.getElementById("username");
 const messageInput = document.getElementById("message");
 const sendButton = document.getElementById("send");
+const username = prompt("Enter your name.");
 
+// Check if user entered a valid name to chat, if not it will reload the page.
+if (!username || username.trim() === "") {
+  alert("You need to enter valid name, To Start chat.");
+  location.reload();
+}
+
+// To display the message in the page
 ws.onmessage = async (event) => {
   let messageContent;
 
@@ -17,12 +24,21 @@ ws.onmessage = async (event) => {
   message.textContent = messageContent;
   chat.appendChild(message);
 };
-
+// to send message whan the user click send button
 sendButton.addEventListener("click", () => {
-  const username = usernameInput.value.trim();
-  const message = messageInput.value.trim();
+  let message = messageInput.value.trim();
   if (username && message) {
     ws.send(`${username}: ${message}`);
     messageInput.value = "";
+  }
+});
+// to send message whan the user click Enter in keybord
+document.addEventListener("keyup", (key) => {
+  let message = messageInput.value.trim();
+  if (key.code === "Enter") {
+    if (username && message) {
+      ws.send(`${username}: ${message}`);
+      messageInput.value = "";
+    }
   }
 });
